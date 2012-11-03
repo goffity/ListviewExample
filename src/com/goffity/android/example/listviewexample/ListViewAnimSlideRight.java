d147963192fcd503c46dd1a809668f59bed39a06
Goffity <goffity@gmail.com>
2012-11-03 14:56:50 +0700

    Added animation "slide right out" in new Activity

--- src/com/goffity/android/example/listviewexample/ListViewAnimSlideRight.java
+++ src/com/goffity/android/example/listviewexample/ListViewAnimSlideRight.java
@@ -3,35 +3,32 @@ package com.goffity.android.example.listviewexample;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
-public class MainActivity extends Activity {
+public class ListViewAnimSlideRight extends Activity {
 
 	ArrayAdapter<String> adapter;
-	ArrayAdapter<String> topDownAdapter;
 
 	List<String> listValue;
-	String[] values;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_list_view_anim_slide_right);
 
-		ListView listView = (ListView) findViewById(R.id.mylist);
+		ListView listView = (ListView) findViewById(R.id.listViewSlideOutRight);
 
 		listValue = new ArrayList<String>();
 		listValue.add("Android");
@@ -45,40 +42,26 @@ public class MainActivity extends Activity {
 		listValue.add("Linux");
 		listValue.add("OS/2");
 
-		// First paramenter - Context
-		// Second parameter - Layout for the row
-		// Third parameter - ID of the TextView to which the data is written
-		// Forth - the Array of data
 		adapter = new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_1, android.R.id.text1,
 				listValue);
-
-		// Assign adapter to ListView
 		listView.setAdapter(adapter);
-
 		listView.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
-			public void onItemClick(AdapterView<?> parent, View rowView,
+			public void onItemClick(AdapterView<?> arparentg0, View rowView,
 					int positon, long id) {
-				Toast.makeText(
-						rowView.getContext(),
-						"Position: " + positon + ": "
-								+ listValue.get(positon).toString(),
-						Toast.LENGTH_SHORT).show();
-
+				// TODO Auto-generated method stub
 				removeListItem(rowView, positon);
 
 			}
-
 		});
-
 	}
 
 	protected void removeListItem(View rowView, final int positon) {
 		// TODO Auto-generated method stub
 		final Animation animation = AnimationUtils.loadAnimation(
-				rowView.getContext(), R.anim.splashfadeout);
+				rowView.getContext(), android.R.anim.slide_out_right);
 		rowView.startAnimation(animation);
 		Handler handle = new Handler();
 		handle.postDelayed(new Runnable() {
@@ -91,13 +74,14 @@ public class MainActivity extends Activity {
 				adapter.notifyDataSetChanged();
 				animation.cancel();
 			}
-		}, 1000);
+		}, 450);
 
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_main, menu);
+		getMenuInflater().inflate(R.menu.activity_list_view_anim_slide_right,
+				menu);
 		return true;
 	}
 
@@ -105,9 +89,9 @@ public class MainActivity extends Activity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// TODO Auto-generated method stub
 		switch (item.getItemId()) {
-		case R.id.menu_silde_right:
+		case R.id.menu_main:
 			Intent intent = new Intent(getApplicationContext(),
-					ListViewAnimSlideRight.class);
+					MainActivity.class);
 			this.startActivity(intent);
 			break;
 		default:
